b2331311ab54365c570954285fd05f2c4329202e
Prabhakaran <prabhakaran2002k@gmail.com>
2025-08-28 15:52:27 +0530

    sky routes

---
+++ src/app/product/raw-material/raw-material.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-raw-material',
+  imports: [],
+  templateUrl: './raw-material.component.html',
+  styleUrl: './raw-material.component.scss'
+})
+export class RawMaterialComponent {
+
+}
