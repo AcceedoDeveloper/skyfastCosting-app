1b043c40cee7cdb4047e4704ce93a63b8e0faa10
Sriram <mssabari503@gmail.com>
2025-12-26 16:07:38 +0530

    version ui update

--- src/app/master/system-organization/user-management-update/user-management-update.component.ts
+++ src/app/master/system-organization/user-management-update/user-management-update.component.ts
@@ -123,14 +123,15 @@ scrollTo(anchor: string): void {
     return this.sanitizer.bypassSecurityTrustStyle(style);
   }
 
-  getImageUrl(filename: string): string {
+    getImageUrl(filename: string): string {
     if (!filename) return '';
     if (/^https?:\/\//.test(filename)) return filename;
 
-    const apiUrl = this.config.getCostingUrl('getVersions');
-    const urlObj = new URL(apiUrl);
-    const baseUrl = `${urlObj.protocol}//${urlObj.host}`;
-    return `${baseUrl}/uploads/version/${filename}`;
+    const apiUrl = this.config.getCostingUrl('baseUrl');
+    console.log('stand api', apiUrl);
+    // const baseUrl = apiUrl;
+    console.log('Constructed base URL for image:', `${apiUrl}uploads/version/${filename}`);
+    return `${apiUrl}uploads/version/${filename}`;
   }
 
   openLightbox(url: string, title?: string): void {
